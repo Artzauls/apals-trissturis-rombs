ff3b8576b965d054b0958ad1d090c369ecf8b1a8
ricards960 <98381706+Artzauls@users.noreply.github.com>
2022-02-01 10:30:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle, random
+apals = turtle.Turtle()
+apals.color("pink")
+apals.up()
+apals.setpos(100, -21)
+apals.down()
+apals.circle(70)
+
+trissturis = turtle.Turtle()
+trissturis.color("blue")
+trissturis.setpos(0, -1)
+trissturis.circle(20, steps=3)
+
+rombs =turtle.Turtle()
+rombs.color("black")
+rombs.up()
+rombs.setpos(90, -120)
+rombs.down()
+rombs.circle(40, steps=4)
